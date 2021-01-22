cd19f271ee4aa0728464a900b6ae54955e9f504d
matthuntsberry <matthew.marshall.huntsberry@ibm.com>
2021-01-21 18:44:12 -0700

    Fix: Add cors import

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const books = require("./routers/books");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(books);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the books api");
+  res.send("Welcome to the books API");
 });
 
-app.listen(PORT, console.log(`I am listening on port ${5000}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- routers/books/index.js
+++ routers/books/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const booksControllers = require("../../controllers/books/books");
+const booksControllers = require("../../controllers/books");
 
 router.get("/books", booksControllers.list);
 
